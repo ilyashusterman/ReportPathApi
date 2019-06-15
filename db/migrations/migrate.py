3c5fe3d58ba5d5b2fb2c98ffe041494e22077efe
Ilya Shusterman <shusterilyaman@gmail.com>
2019-06-15 15:44:56 +0300

    fix: updated todos

--- db/migrations/migrate.py
+++ db/migrations/migrate.py
@@ -1,4 +1,5 @@
-#TODO script that initiate all migrations (create tables , unique constraints)
+#TODO script that initiate all migrations (for minimal constrains required )
+# (create tables , unique constraints)
 # for version nums
 # 1. mongodb - migrations
 # 2. firebase - migrations
